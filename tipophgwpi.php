727ddd11703b71e340676d66e5223cf8b119f5f2
phgwpi <phgwpi@gmail.com>
2015-12-21 23:35:48 +0100

    First

---
+++ tipophgwpi.php
@@ -0,0 +1,412 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="dcterms.created" content="vi., 27 nov. 2015 06:46:43 GMT">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>tipografías</title>
+    <style>
+    
+    body{text-align: center;
+	     width:70%; margin:auto;
+		 }
+	
+	/********************************************************************************/
+	
+	   .aaargh{font-family: 'aaarghnormal';}
+	   
+	   .jurademibold{font-family: 'jurademibold'; }
+	   .juralight{font-family: 'juralight'; }
+	   .juramedium{font-family: 'juramedium'; }
+	   .jurabook{font-family: 'jurabook';}
+	   
+	   .pompiere_regular{font-family: 'pompiere_regular';}
+	   
+	   .bonvenocflight{font-family: 'bonvenocflight';background: #FFCCCC;}
+	   
+	   .idolwildregular{ font-family: 'idolwildregular';}
+	   
+	   .short_stackregular{font-family: 'short_stackregular';background: #FFCCCC;}
+	   
+	   .blokletters_balpenbalpen{font-family: 'blokletters_balpenbalpen';}
+	   .blokletters_potloodpotlood{font-family:'blokletters_potloodpotlood';background: #FFCCCC;}
+	   .blokletters_viltstifviltstift{font-family: 'blokletters_viltstifviltstift';}
+	   
+	   .arconregular{font-family: 'arconregular';}
+	   .arconrounded-regular{font-family: 'arconrounded-regular';background: #FFCCCC;}
+
+     .kelly_slabregular{font-family: 'kelly_slabregular';}
+
+     .architects_daughterregular{font-family: 'architects_daughterregular';}
+	   
+	   
+	   
+	/*********************************************************************************/
+
+	@font-face {
+    font-family: 'aaarghnormal';
+    src: url('tipophgwpi/aaargh-webfont.eot');
+    src: url('tipophgwpi/aaargh-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/aaargh-webfont.woff2') format('woff2'),
+         url('tipophgwpi/aaargh-webfont.woff') format('woff'),
+         url('tipophgwpi/aaargh-webfont.ttf') format('truetype'),
+         url('tipophgwpi/aaargh-webfont.svg#aaarghnormal') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+    
+	/**************************************************************************************/
+	   
+	   @font-face {
+    font-family: 'jurademibold';
+    src: url('tipophgwpi/jura-demibold-webfont.eot');
+    src: url('tipophgwpi/jura-demibold-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/jura-demibold-webfont.woff2') format('woff2'),
+         url('tipophgwpi/jura-demibold-webfont.woff') format('woff'),
+         url('tipophgwpi/jura-demibold-webfont.ttf') format('truetype'),
+         url('tipophgwpi/jura-demibold-webfont.svg#jurademibold') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'juralight';
+    src: url('tipophgwpi/jura-light-webfont.eot');
+    src: url('tipophgwpi/jura-light-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/jura-light-webfont.woff2') format('woff2'),
+         url('tipophgwpi/jura-light-webfont.woff') format('woff'),
+         url('tipophgwpi/jura-light-webfont.ttf') format('truetype'),
+         url('tipophgwpi/jura-light-webfont.svg#juralight') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'juramedium';
+    src: url('tipophgwpi/jura-medium-webfont.eot');
+    src: url('tipophgwpi/jura-medium-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/jura-medium-webfont.woff2') format('woff2'),
+         url('tipophgwpi/jura-medium-webfont.woff') format('woff'),
+         url('tipophgwpi/jura-medium-webfont.ttf') format('truetype'),
+         url('tipophgwpi/jura-medium-webfont.svg#juramedium') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+@font-face {
+    font-family: 'jurabook';
+    src: url('tipophgwpi/jura-regular-webfont.eot');
+    src: url('tipophgwpi/jura-regular-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/jura-regular-webfont.woff2') format('woff2'),
+         url('tipophgwpi/jura-regular-webfont.woff') format('woff'),
+         url('tipophgwpi/jura-regular-webfont.ttf') format('truetype'),
+         url('tipophgwpi/jura-regular-webfont.svg#jurabook') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+/***********************/
+
+@font-face {
+    font-family: 'pompiere_regular';
+    src: url('tipophgwpi/pompiere-regular-webfont.eot');
+    src: url('tipophgwpi/pompiere-regular-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/pompiere-regular-webfont.woff2') format('woff2'),
+         url('tipophgwpi/pompiere-regular-webfont.woff') format('woff'),
+         url('tipophgwpi/pompiere-regular-webfont.ttf') format('truetype'),
+         url('tipophgwpi/pompiere-regular-webfont.svg#pompiere_regular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+/**************************************************************/
+
+@font-face {
+    font-family: 'bonvenocflight';
+    src: url('tipophgwpi/bonvenocf-light-webfont.eot');
+    src: url('tipophgwpi/bonvenocf-light-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/bonvenocf-light-webfont.woff2') format('woff2'),
+         url('tipophgwpi/bonvenocf-light-webfont.woff') format('woff'),
+         url('tipophgwpi/bonvenocf-light-webfont.ttf') format('truetype'),
+         url('tipophgwpi/bonvenocf-light-webfont.svg#bonvenocflight') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+/******************************************************************************/
+
+@font-face {
+    font-family: 'idolwildregular';
+    src: url('tipophgwpi/idolwild-webfont.eot');
+    src: url('tipophgwpi/idolwild-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/idolwild-webfont.woff2') format('woff2'),
+         url('tipophgwpi/idolwild-webfont.woff') format('woff'),
+         url('tipophgwpi/idolwild-webfont.ttf') format('truetype'),
+         url('tipophgwpi/idolwild-webfont.svg#idolwildregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+
+/***********************************************************************************/
+@font-face {
+    font-family: 'short_stackregular';
+    src: url('tipophgwpi/shortstack-regular-webfont.eot');
+    src: url('tipophgwpi/shortstack-regular-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/shortstack-regular-webfont.woff2') format('woff2'),
+         url('tipophgwpi/shortstack-regular-webfont.woff') format('woff'),
+         url('tipophgwpi/shortstack-regular-webfont.ttf') format('truetype'),
+         url('tipophgwpi/shortstack-regular-webfont.svg#short_stackregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+/************************************************************************************/
+		@font-face {
+    font-family: 'blokletters_balpenbalpen';
+    src: url('tipophgwpi/blokletters-balpen-webfont.eot');
+    src: url('tipophgwpi/blokletters-balpen-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/blokletters-balpen-webfont.woff2') format('woff2'),
+         url('tipophgwpi/blokletters-balpen-webfont.woff') format('woff'),
+         url('tipophgwpi/blokletters-balpen-webfont.ttf') format('truetype'),
+         url('tipophgwpi/blokletters-balpen-webfont.svg#blokletters_balpenbalpen') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+
+
+
+@font-face {
+    font-family: 'blokletters_potloodpotlood';
+    src: url('tipophgwpi/blokletters-potlood-webfont.eot');
+    src: url('tipophgwpi/blokletters-potlood-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/blokletters-potlood-webfont.woff2') format('woff2'),
+         url('tipophgwpi/blokletters-potlood-webfont.woff') format('woff'),
+         url('tipophgwpi/blokletters-potlood-webfont.ttf') format('truetype'),
+         url('tipophgwpi/blokletters-potlood-webfont.svg#blokletters_potloodpotlood') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+
+
+
+@font-face {
+    font-family: 'blokletters_viltstifviltstift';
+    src: url('tipophgwpi/blokletters-viltstift-webfont.eot');
+    src: url('tipophgwpi/blokletters-viltstift-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/blokletters-viltstift-webfont.woff2') format('woff2'),
+         url('tipophgwpi/blokletters-viltstift-webfont.woff') format('woff'),
+         url('tipophgwpi/blokletters-viltstift-webfont.ttf') format('truetype'),
+         url('tipophgwpi/blokletters-viltstift-webfont.svg#blokletters_viltstifviltstift') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+/**********************************************************************************/
+
+
+@font-face {
+    font-family: 'arconregular';
+    src: url('tipophgwpi/arcon-regular-webfont.eot');
+    src: url('tipophgwpi/arcon-regular-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/arcon-regular-webfont.woff2') format('woff2'),
+         url('tipophgwpi/arcon-regular-webfont.woff') format('woff'),
+         url('tipophgwpi/arcon-regular-webfont.ttf') format('truetype'),
+         url('tipophgwpi/arcon-regular-webfont.svg#arconregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+
+
+
+@font-face {
+    font-family: 'arconrounded-regular';
+    src: url('tipophgwpi/arcon-rounded-regular-webfont.eot');
+    src: url('tipophgwpi/arcon-rounded-regular-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/arcon-rounded-regular-webfont.woff2') format('woff2'),
+         url('tipophgwpi/arcon-rounded-regular-webfont.woff') format('woff'),
+         url('tipophgwpi/arcon-rounded-regular-webfont.ttf') format('truetype'),
+         url('tipophgwpi/arcon-rounded-regular-webfont.svg#arconrounded-regular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+/*****************************************************************************/
+
+@font-face {
+    font-family: 'kelly_slabregular';
+    src: url('tipophgwpi/kellyslab-regular-webfont.eot');
+    src: url('tipophgwpi/kellyslab-regular-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/kellyslab-regular-webfont.woff2') format('woff2'),
+         url('tipophgwpi/kellyslab-regular-webfont.woff') format('woff'),
+         url('tipophgwpi/kellyslab-regular-webfont.ttf') format('truetype'),
+         url('tipophgwpi/kellyslab-regular-webfont.svg#kelly_slabregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+/*********************************************************************************/
+
+@font-face {
+    font-family: 'architects_daughterregular';
+    src: url('tipophgwpi/architectsdaughter-webfont.eot');
+    src: url('tipophgwpi/architectsdaughter-webfont.eot?#iefix') format('embedded-opentype'),
+         url('tipophgwpi/architectsdaughter-webfont.woff2') format('woff2'),
+         url('tipophgwpi/architectsdaughter-webfont.woff') format('woff'),
+         url('tipophgwpi/architectsdaughter-webfont.ttf') format('truetype'),
+         url('tipophgwpi/architectsdaughter-webfont.svg#architects_daughterregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+
+}
+
+/******************************************************************************************/
+   
+	</style>
+  
+  </head>
+  <body>
+  
+  <h1>Tipografías phgwpi</h1>
+  
+  
+  <section>
+  
+  <div class="aaargh">
+  <h2>Aaargh</h2>
+  
+  
+  <div id="demo"><p>Let AJAX change this text</p></div>
+
+<button type="button" onclick="loadDoc()">Change Content</button>
+
+<script>
+ function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "tipophgwpi/ajax_info.txt", true);
+  xhttp.send();
+}
+</script>
+</div>
+  
+  <hr>
+  </section>
+  
+  <section>
+  
+  <div class="jurademibold">
+  <h3>Jurademibold</h3>
+  <p>AJAX allows web pages to be updated asynchronously by exchanging small amounts of data with the server 
+  behind the scenes. This means that it is possible to update parts of a web page, without reloading the whole page.</p>  
+  </div>
+  
+  <div class="juralight">
+  <h3>Juralight</h3>
+  <p>The XMLHttpRequest object is used to exchange data with a server behind the scenes. This means that it is possible 
+  to update parts of a web page, without reloading the whole page.</p>
+  </div>
+  
+  <div class="juramedium">
+  <h3>Juramedium</h3>
+  <p>Syntax for creating an XMLHttpRequest object:variable = new XMLHttpRequest();</p>
+  </div>
+  
+  <div class="jurabook">
+  <h3>Jurabook</h3>
+  <p>To send a request to a server, we use the open() and send() methods of the XMLHttpRequest object:
+  xhttp.open("GET", "ajax_info.txt", true);<br>
+  xhttp.send();</p>
+  </div>
+  
+  <div class="pompiere_regular">
+ <h3>pompiere_regular</h3>
+  <p>GET is simpler and faster than POST, and can be used in most cases. However, always use POST requests when:
+  A cached file is not an option (update a file or database on the server). Sending a large amount of data to the server 
+  (POST has no size limitations). Sending user input (which can contain unknown characters), POST is more robust and secure than GET.</p>
+  </div>
+  
+  <div class="bonvenocflight">
+  <h3>bonvenocflight: <strong>afp5music main and foro</strong></h3    
+  <p>To get the response from a server, use the responseText or responseXML property of the XMLHttpRequest object.</p>  
+  </div>
+    
+	
+	<div class="idolwildregular">
+	<h3>Idolwildregular</h3
+	
+	<p>responseText: Get the response data as a string. </p>
+	<p>ResponseXML: Get the response data as XML data</p>
+	
+	</div>
+	
+	<div class="short_stackregular">
+	<h3>short_stackregular: Mezoitronic</h3>
+	<p>If the response from the server is XML, and you want to parse it as an XML object, use the responseXML property:</p>
+	</div>
+	
+	<div class="blokletters_balpenbalpen">
+	<h3>blokletters_balpenbalpen</h3>
+	<p>When a request to a server is sent, we want to perform some actions based on the response.</p>
+	</div>
+	
+	<div class="blokletters_potloodpotlood">
+	<h3>blokletters_potloodpotlood:Opereta</h3>
+	<p>The onreadystatechange event is triggered every time the readyState changes.</p>
+	</div>
+	
+	<div class="blokletters_viltstifviltstift">
+	<h3>blokletters_viltstifviltstift</h3>
+	<p>The readyState property holds the status of the XMLHttpRequest.</p>
+	</div>
+	
+	<div class="arconregular">
+	<h3>arconregular</h3>
+	<p>Three important properties of the XMLHttpRequest object:<strong>onreadystatechange</strong> stores a function (or the name 
+	   of a function) to be called automatically each time the readyState property changes</p>
+	   <p><strong>readyState</strong> holds the status of the XMLHttpRequest. Changes from 0 to 4: 0: request not initialized , 1: server 
+	   connection established  2: request received, 3: processing request , 4: request finished and response is ready. 
+	    <strong>Status</strong>200: "OK", 404: Page not found</p>
+	</div>
+	
+	<div class="arconrounded-regular">
+	<h3>arconrounded-regular: <strong>Versiones</strong></h3>
+	<p>In the onreadystatechange event, we specify what will happen when the server response is ready to be processed.
+        When readyState is 4 and status is 200, the response is ready:</p>
+	</div>
+
+  <div class="kelly_slabregular">
+  <h3>kelly_slabregular</h3>
+  <p>Using a Callback Function: A callback function is a function passed as a parameter to another function.
+  If you have more than one AJAX task on your website, you should create ONE standard function for creating 
+  the XMLHttpRequest object, and call this for each AJAX task.</p>
+  </div>
+
+  <div class="architects_daughterregular">
+  <h3>architects_daughterregular</h3>
+  <p>In the example above, when a user types a character in the input field, a function called "showHint()" is executed.
+The function is triggered by the onkeyup event.</p>
+  </div>
+	
+  </section>
+  
+  
+  </body>
+</html>
